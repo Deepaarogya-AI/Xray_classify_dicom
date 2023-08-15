2ad3a5bef703caf34978a08a27727ff8a116ba7b
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-08-15 12:20:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,28 @@
 
 
 import os
-from flask import Flask, render_template, request
-import torch
-import torch.nn.functional as F
-from torchvision.transforms import transforms
-from torchvision.models import resnet50, densenet161, resnet101
-from PIL import Image
-import numpy as np
-#from torchvision.models import convnext_small 
-from torchvision.models import efficientnet_b4,resnet34
-from torchvision.models import resnet50,densenet161,inception_v3,regnet_y_8gf
-from torchvision.models import efficientnet_b6
-import mritopng
 import shutil
-
-from werkzeug.utils import secure_filename
-from PIL import UnidentifiedImageError
-
-from flask import Flask, render_template, request, send_file  # Add send_file import
-# Other import statements are here...
-
+import base64
+import csv
+import mritopng
+import numpy as np
 import io
 import csv
 import base64
 from io import BytesIO
 import tempfile
+from werkzeug.utils import secure_filename
+from PIL import Image, UnidentifiedImageError
+from flask import Flask, render_template, request, send_file
+
+import torch
+import torch.nn.functional as F
+from torchvision.transforms import transforms
+import torch.nn as nn
+from torchvision.models import resnet50, densenet161, inception_v3, regnet_y_8gf, efficientnet_b4, resnet34, resnet101
+
 
 app = Flask(__name__)
-
 
 
 UPLOAD_FOLDER = 'uploaded_images'  # Directory to save uploaded images
@@ -78,12 +72,6 @@ densenet_model = create_densenet_model()
 resnet34_model=create_resnet34_model()
 
 
-
-
-import torch
-import torch.nn as nn
-from torchvision.models import densenet161
-
 def create_densenet_model(num_classes=2, dropout_rate=0.2):
     model = densenet161(pretrained=False)  # Load the DenseNet model without pretrained weights
     num_features = model.classifier.in_features
@@ -104,8 +92,6 @@ model_dict = model_dens.state_dict()
 pretrained_dict = {k: v for k, v in pretrained_state_dict.items() if k in model_dict}
 model_dict.update(pretrained_dict)
 model_dens.load_state_dict(model_dict)
-
-
 
 
 def create_densenet_aug_model(num_classes=2, dropout_rate=0.2):
@@ -137,8 +123,6 @@ resnet_model.load_state_dict(torch.load('v2_resnet50new_adamW_withcrossenloss94a
 resnet101_model.load_state_dict(torch.load('v2_resnet101_heavydataaug_pretrain.pth', map_location=torch.device('cpu')))
 #densenet_model.load_state_dict(torch.load('v2_densenet121_95acc.pth',map_location=torch.device('cpu')))
     #best_model_DenseNet.pth', map_location=torch.device('cpu')))
-
-
 
 # Define a function to preprocess the input image
 def preprocess_image(image, model_name, image_size):
@@ -208,62 +192,107 @@ def dicom_to_jpg(dicom_file):
         return None
 
 
-CONVERTED_IMAGES_FOLDER = 'converted_images'
+def process_dicom_image(image, ensemble_models, label_map, threshold, image_size):
+    image_filename = secure_filename(image.filename)
+    dicom_path = os.path.join(converted_images_folder, image_filename)
+    image.save(dicom_path)
+    
+    png_path = os.path.join(converted_images_folder, f"{image_filename}.png")
+    mritopng.convert_file(dicom_path, png_path, auto_contrast=True)
+    png_image = Image.open(png_path).convert('RGB')
+    image_for_prediction = png_image
 
-if not os.path.exists(CONVERTED_IMAGES_FOLDER):
-    os.makedirs(CONVERTED_IMAGES_FOLDER)
+    with open(png_path, "rb") as image_file:
+        base64_image_data = base64.b64encode(image_file.read()).decode("utf-8")
+
+    predicted_label_index, predictions = ensemble_inference(ensemble_models, image_for_prediction, image_size)
+    confidence_score = predictions[predicted_label_index]
+
+    if confidence_score > threshold:
+        predicted_label_str = label_map[predicted_label_index]
+    else:
+        predicted_label_str = 'No result'
+
+    prediction_data = {
+        "image_name": image_filename,
+        "predicted_label": predicted_label_str,
+        "confidence_score": confidence_score,
+        "base64_image_data": base64_image_data
+    }
+
+    return prediction_data
+
+
+def process_normal_image(image, label_map, ensemble_models, threshold, image_size):
+    image_filename = secure_filename(image.filename)
+    image_path = os.path.join(converted_images_folder, image_filename)
+    
+    with open(image_path, "wb") as image_file:
+        image.save(image_file)
+    
+    img = Image.open(image_path).convert('RGB')
+
+    predicted_label_index, predictions = ensemble_inference(ensemble_models, img, image_size)
+    confidence_score = predictions[predicted_label_index]
+
+    if confidence_score > threshold:
+        predicted_label_str = label_map[predicted_label_index]
+    else:
+        predicted_label_str = 'No result'
+
+    with open(image_path, "rb") as image_file:
+        base64_image_data = base64.b64encode(image_file.read()).decode("utf-8")
+
+    os.remove(image_path)  # Remove the temporary image file
+
+    prediction_data = {
+        "image_name": image_filename,
+        "predicted_label": predicted_label_str,
+        "confidence_score": confidence_score,
+        "base64_image_data": base64_image_data
+    }
+
+    return prediction_data
+
+
+
+converted_images_folder = 'converted_images'
+
+if not os.path.exists(converted_images_folder):
+    os.makedirs(converted_images_folder)
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        images = request.files.getlist('images')  # Get the list of uploaded images
+        images = request.files.getlist('images')
         if not images:
             return render_template('index.html', message='No files selected')
+
+        # Define the label map, threshold, and image size
+        label_map = {0: 'healthy', 1: 'unhealthy'}
+        threshold = 0.50
+        image_size = (224, 224)
+        ensemble_models = [resnet_model, model_dens, model_dens_aug]
 
         predictions_data = []
         for image in images:
             image_filename = secure_filename(image.filename)
             image_format = image_filename.split('.')[-1].lower()
+
             if image_format == 'dcm':
-                # Convert DICOM to PNG using mritopng
-                dicom_path = os.path.join(CONVERTED_IMAGES_FOLDER, image_filename)
-                image.save(dicom_path)
-                png_path = os.path.join(CONVERTED_IMAGES_FOLDER, f"{image_filename}.png")
-                mritopng.convert_file(dicom_path, png_path, auto_contrast=True)
-                png_image = Image.open(png_path).convert('RGB')
-                image_for_prediction = png_image
 
-                # Read the PNG image and convert to base64-encoded data
-                with open(png_path, "rb") as image_file:
-                    base64_image_data = base64.b64encode(image_file.read()).decode("utf-8")
-
-        
-                # Define the label map
-                label_map = {0: 'healthy', 1: 'unhealthy'}
-
-                threshold = 0.50
-                # Perform ensemble inference with image size (224, 224) for example
-                image_size = (224, 224)  # You can choose a different size as needed
-
-                # Perform ensemble inference and get predictions as before
-                predicted_label_index, predictions = ensemble_inference([resnet_model, model_dens, model_dens_aug], image_for_prediction, image_size)
-                confidence_score = predictions[predicted_label_index]
-
-                # Check if confidence score is above the threshold
-                if confidence_score > threshold:
-                    predicted_label_str = label_map[predicted_label_index]
-                else:
-                    predicted_label_str = 'No result'
-
-                # Store prediction results in a dictionary
-                prediction_data = {
-                    "image_name": image_filename,
-                    "predicted_label": predicted_label_str,
-                    "confidence_score": confidence_score,
-                    "base64_image_data": base64_image_data  # Store the Base64-encoded image data
-                }
-
+                prediction_data = process_dicom_image(image, ensemble_models, label_map, threshold, image_size)
                 predictions_data.append(prediction_data)
 
+            elif image_format in ['jpg', 'jpeg', 'png']:
+                prediction_data = process_normal_image(image, label_map, ensemble_models, threshold, image_size)
+                predictions_data.append(prediction_data)
+
+            else:
+                print(f"Unsupported image format: {image_format}")
+                continue
 
         # Save the results to a CSV file
         save_to_csv(predictions_data)
@@ -326,8 +355,6 @@ def save_to_csv(predictions_data):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 
 # Use an official Python runtime as a parent image
